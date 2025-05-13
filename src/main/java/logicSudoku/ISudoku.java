1ec5955613a6b5695e611e6f3967492d5adc4cff
FMontes-alt <franciscomontes95@gmail.com>
2025-05-13 17:57:56 +0200

    Creacion de la Interfaz ISudoku

---
+++ src/main/java/logicSudoku/ISudoku.java
@@ -0,0 +1,9 @@
+package logicSudoku;
+
+public interface ISudoku {
+    void generarTablero (String dificultad);
+    boolean esMovimientoValido (int fila, int columna, int valor);
+    boolean colocarNumero (int fila,, int columna, int valor);
+    boolean estaResuelto();
+    void mostarTablero();
+}
